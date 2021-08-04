9b898ac2884f49fb53bdb5780986ac722d0d50e9
Artemii Chernega <a.chernega2000@gmail.com>
2021-08-04 18:36:37 +0300

    firebase auth / google sign in

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCqcnZpp2_8H_dkRwyqSCNtccjK6oFARIk",
+  authDomain: "ecommerce-website-d9401.firebaseapp.com",
+  projectId: "ecommerce-website-d9401",
+  storageBucket: "ecommerce-website-d9401.appspot.com",
+  messagingSenderId: "37760826979",
+  appId: "1:37760826979:web:57c97777e4e847c49f4256",
+};
